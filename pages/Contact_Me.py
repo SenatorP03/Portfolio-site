9af3f7b276735ee2e269214934f8be55477af310
Adeiye Oluwasegun <126037893+SenatorP03@users.noreply.github.com>
2024-06-03 06:04:08 +0100

    Update Contact_Me.py

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -3,8 +3,8 @@ from send_email import send_email
 
 st.header("Contact Me")
 with st.form(key="emai_forms"):
-    user_email = st.text_input("enter an email address")
-    raw_message = st.text_area("Your comment here")
+    user_email = st.text_input("Email address")
+    raw_message = st.text_area("Your message here")
     message = (f"""\
 Subject:New email from {user_email}
     
